b62c777e8d42b54e78a9d03af71af5e21584eaf0
poronite <72969946+poronite@users.noreply.github.com>
2021-09-29 22:35:50 +0100

    Small Typing prototype
    Made a small prototype for the typing mechanic.

---
+++ Type of Adventure/Assets/Scripts/Typing.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Typing : MonoBehaviour
+{
+    //Text bank
+
+    public Text wordOutput, numMistakesOutput;
+
+    private string remainingWord, currentWord = "testing";
+
+    private int numMistakes;
+
+
+    void Start()
+    {
+        SetCurrentWord();
+    }
+
+    private void SetCurrentWord()
+    {
+        // get text bank
+        SetRemainingWord(currentWord);
+    }
+
+    private void SetRemainingWord(string word)
+    {
+        remainingWord = word;
+        wordOutput.text = remainingWord.ToUpper();
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+
+    private void CheckInput()
+    {
+        if (Input.anyKeyDown)
+        {
+            string key = Input.inputString;
+
+            if (key.Length == 1)
+            {
+                EnterLetter(key);
+            }
+        }
+    }
+
+    private void EnterLetter(string typedletter)
+    {
+        if (remainingWord.IndexOf(typedletter) == 0) //check if typedletter is correct
+        {
+            RemoveLetter();
+
+            if (remainingWord.Length == 0) //is word complete?
+            {
+                SetCurrentWord();
+            }
+        }
+        else //wrong letter typed
+        {
+            numMistakes++;
+            numMistakesOutput.text = $"Number of Mistakes: {numMistakes}";
+        }
+    }
+
+    private void RemoveLetter()
+    {
+        string newWord = remainingWord.Remove(0, 1);
+        SetRemainingWord(newWord);
+    }   
+}
