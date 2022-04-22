ba73342dd23b623b17ce1b47cca36b015b9410ed
poronite <72969946+poronite@users.noreply.github.com>
2022-04-22 11:07:10 +0100

    Main Menu (Part 1)

---
+++ Assets/Scripts/Menus/Main_Menu_Buttons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Main_Menu_Buttons : MonoBehaviour
+{
+    private GameObject lastSelectedButton;
+
+    
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject != null)
+        {
+            lastSelectedButton = EventSystem.current.currentSelectedGameObject;
+        }
+        else
+        {
+            EventSystem.current.SetSelectedGameObject(lastSelectedButton);
+        }
+    }
+}
