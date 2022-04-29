07ef5a95bc01d17f06d4d899fad7dcd72e0f5302
poronite <72969946+poronite@users.noreply.github.com>
2022-04-29 13:06:54 +0100

    Options Menu (Part 1)

---
+++ Assets/Scripts/Menus/Main_Menu.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using ATOA;
+
+public class Main_Menu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject MainMenu;
+
+    [SerializeField]
+    private GameObject OptionsMenu;
+
+    [SerializeField]
+    private Button playButton;
+
+    [SerializeField]
+    private CanvasGroup fadeIn;
+
+    [SerializeField]
+    private RectTransform pointer;
+
+    [SerializeField]
+    private EventSystem input;
+
+    private GameObject lastSelectedUIMainMenu;
+
+
+    private void Start()
+    {
+        lastSelectedUIMainMenu = playButton.gameObject;
+    }
+
+    public void Play()
+    {
+        input.sendNavigationEvents = false;
+        MainMenu.SetActive(false);
+        StartCoroutine(LoadGame());
+    }
+
+    IEnumerator LoadGame()
+    {
+        yield return StartCoroutine(ATOA_Utilities.FadeLoadingScreen(1f, 1f, fadeIn));
+
+        //After fade in
+        SceneManager.LoadSceneAsync(1);
+    }
+
+
+    public void Options()
+    {
+        Debug.Log("Open Options Menu");
+        MainMenu.SetActive(false);
+        OptionsMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+    void Update()
+    {
+        if (MainMenu.activeInHierarchy == true)
+        {
+            if (EventSystem.current.currentSelectedGameObject != null)
+            {
+                lastSelectedUIMainMenu = EventSystem.current.currentSelectedGameObject;
+                pointer.position = new Vector3(pointer.position.x,
+                    lastSelectedUIMainMenu.transform.position.y,
+                    pointer.position.z);
+            }
+            else
+            {
+                EventSystem.current.SetSelectedGameObject(lastSelectedUIMainMenu);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/Options_Menu.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using ATOA;
+
+public class Options_Menu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject MainMenu;
+
+    [SerializeField]
+    private GameObject OptionsMenu;
+
+    [SerializeField]
+    private Toggle fullscreenToggle;
+
+    [SerializeField]
+    private Slider barOpacitySlider;
+
+    [SerializeField]
+    private Slider musicVolumeSlider;
+
+    [SerializeField]
+    private Slider sfxVolumeSlider;
+
+    [SerializeField]
+    private Slider voiceVolumeSlider;
+
+    private GameObject lastSelectedUIOptionsMenu;
+
+
+    private void Start()
+    {
+        lastSelectedUIOptionsMenu = fullscreenToggle.gameObject;
+    }
+
+
+    public void Back()
+    {
+        Debug.Log("Return to Main Menu");
+        OptionsMenu.SetActive(false);
+        MainMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        lastSelectedUIOptionsMenu = fullscreenToggle.gameObject;
+    }
+
+
+    void Update()
+    {
+        if (OptionsMenu.activeInHierarchy == true)
+        {
+            //Debug.Log(EventSystem.current.currentSelectedGameObject);
+            if (EventSystem.current.currentSelectedGameObject != null)
+            {
+                lastSelectedUIOptionsMenu = EventSystem.current.currentSelectedGameObject;
+                
+            }
+            else
+            {
+                EventSystem.current.SetSelectedGameObject(lastSelectedUIOptionsMenu);
+            }
+        }
+    }
+}
